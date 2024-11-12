b4607998bf7392534860a907fe81ea560d6228b2
Sudré <114266347+JoaoSudre@users.noreply.github.com>
2024-11-12 12:49:52 -0300

    Delete

--- Cálculo IMC/result-script.js
+++
@@ -1,9 +0,0 @@
-// Ao carregar a página de resultados
-document.addEventListener('DOMContentLoaded', function() {
-    const resultado = localStorage.getItem('resultado');
-    const dica = localStorage.getItem('dica');
-
-    // Exibe os resultados e dicas
-    document.getElementById('resultado').textContent = resultado;
-    document.getElementById('dica').textContent = dica;
-});
